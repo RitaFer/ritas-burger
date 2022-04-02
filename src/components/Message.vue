16581bee79fd94c938df9aa4689dd88bb23e5c24
RitaFer <rialf.ferreira@gmail.com>
2022-04-01 21:25:51 -0300

    Create Message.vue

---
+++ src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="message-container">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Message",
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped>
+  .message-container {
+    color: #048500;
+    background-color: #ccffd9;
+    border: 2px solid #b8ffca;
+    border-radius: 5px;
+    padding: 10px;
+    max-width: 80vw;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>
