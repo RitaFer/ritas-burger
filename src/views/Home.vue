85fe995728fa1b000843b6afcd300c1dfa6cae11
RitaFer <rialf.ferreira@gmail.com>
2022-04-01 21:26:50 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="main-container">
-    <h1>Monte o seu burger: </h1>
+  <div id='home' class="main-container">
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+import BurgerForm from "../components/BurgerForm.vue"
 
+export default {
+  name: "Home",
+  components: {
+    BurgerForm
+  }
+}
 </script>
+
+<style scoped>
+  #home {
+    background-image: url(C:\Users\RitaF\Documents\projetos\ritas-burger\public\img\burger.jpg);
+    background-size: cover;
+  }
+</style>
