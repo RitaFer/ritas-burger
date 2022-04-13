9d7f266e03ce97c02d91dcbaa37451cb5709cc1c
RitaFer <rialf.ferreira@gmail.com>
2022-04-13 10:03:50 -0300

    Update server.js
    bug f5 correction

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var serveStatic = require('serve-static')
 
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
+app.route('/requests')
+    .get(function(req, res) {
+          res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
 
 var port = process.env.PORT || 5000
 app.listen(port)
-console.log('server started' + port)
+console.log('Servidor rodando na porta: ' + port)
