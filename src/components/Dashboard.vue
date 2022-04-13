58905093b7d6ad3caf40c9ed6b69e73579629d19
RitaFer <rialf.ferreira@gmail.com>
2022-04-13 16:46:34 -0300

    update dashboard-requests

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,67 +1,78 @@
 <template>
-  <div id="burger-table">
-    <div>
-      <Message :msg="msg" v-show="msg" />
-      <div id="burger-table-heading">
-        <div class="order-id">#</div>
-        <div>Cliente</div>
-        <div>Pão</div>
-        <div>Modo</div>
-        <div>Hamburguer</div>
-        <div>Temperos</div>
-        <div>Frios</div>
-        <div>Salada</div>
-        <div>Extras</div>
-        <div>Molhos</div>
+<div>
+  <Message :msg="msg" v-show="msg" />
+  <div class="dash">
+    <div class="card" v-for="request in requests" :key="request.id">
+      <div class="id">
+        <span>Pedido nº:</span>
+        <p>{{ request.id }}</p>
       </div>
-    </div>
-    <div id="burger-table-rows">
-      <div class="burger-table-row" v-for="request in requests" :key="request.id">
-        <div class="order-number">{{ request.id }}</div>
-        <div>{{ request.nameRequest }}</div>
-        <div>{{ request.breadTypeRequest }}</div>
-        <div>{{ request.breadModeRequest }}</div>
-        <div>{{ request.burgerTypeRequest }}</div>
-        <div>
-          <ul v-for="(checkedSpices, index) in request.checkedSpices" :key="index">
-            <li>{{ checkedSpices }}</li>
-          </ul>
+      <div class="name">
+        <p>{{ request.nameRequest }}</p>
+      </div>
+      <div class="main">
+        <div class="right">
+          <div class="breadType">
+            <span>Tipo do Pão: </span>
+            <p>{{ request.breadTypeRequest }}</p>
+          </div>
+          <div class="burgerType">
+            <span>Hamburguer: </span>
+            <p>{{ request.burgerTypeRequest }}</p>
+          </div>
+          <div class="coldCuts">
+            <span>Frios: </span>
+            <ul v-for="(checkedColdCuts, index) in request.checkedColdCuts" :key="index">
+              <li>{{ checkedColdCuts }}</li>
+            </ul>
+          </div>
+          <div class="add">
+            <span>Extras: </span>
+            <ul v-for="(checkedExtras, index) in request.checkedExtras" :key="index">
+              <li>{{ checkedExtras }}</li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <ul v-for="(checkedColdCuts, index) in request.checkedColdCuts" :key="index">
-            <li>{{ checkedColdCuts }}</li>
-          </ul>
-        </div>
-        <div>
-          <ul v-for="(checkedSalad, index) in request.checkedSalad" :key="index">
-            <li>{{ checkedSalad }}</li>
-          </ul>
-        </div>
-        <div>
-          <ul v-for="(checkedExtras, index) in request.checkedExtras" :key="index">
-            <li>{{ checkedExtras }}</li>
-          </ul>
-        </div>
-        <div>
-          <ul v-for="(checkedSauces, index) in request.checkedSauces" :key="index">
-            <li>{{ checkedSauces }}</li>
-          </ul>
+        <div class="left">
+          <div class="breadMode">
+            <span>Modo do Pão: </span>
+            <p>{{ request.breadModeRequest }}</p>
+          </div>
+          <div class="spices">
+            <span>Temperos: </span>
+            <ul v-for="(checkedSpices, index) in request.checkedSpices" :key="index">
+              <li>{{ checkedSpices }}</li>
+            </ul>
+          </div>
+          <div class="salad">
+            <span>Salada: </span>
+            <ul v-for="(checkedSalad, index) in request.checkedSalad" :key="index">
+              <li>{{ checkedSalad }}</li>
+            </ul>
+          </div>
+          <div class="sauces">
+            <span>Molhos: </span>
+            <ul v-for="(checkedSauces, index) in request.checkedSauces" :key="index">
+              <li>{{ checkedSauces }}</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="action" v-for="request in requests" :key="request.id">
-      <select name="status" class="status" @change="updateBurger($event, request.id)">
-        <option :value="s.type" v-for="s in status" :key="s.id" :selected="request.statusRequest == s.type">{{ s.type }}</option>
-      </select>
-      <button class="delete-btn" @click="deleteBurger(request.id)">Cancelar Pedido</button>
+      <div class="action">
+        <select name="status" class="status" @change="updateBurger($event, request.id)">
+          <option :value="s.type" v-for="s in status" :key="s.id" :selected="request.statusRequest == s.type">{{ s.type }}</option>
+        </select>
+        <button class="delete-btn" @click="deleteBurger(request.id)">Cancelar Pedido</button>
+      </div>
     </div>
   </div>
+</div>
+      
 </template>
 <script>
-import Message from './Message'
+import Message from './Message.vue'
 
 export default {
-  name: "Dashboard",
   data() {
       return {
         requests: null,
@@ -115,63 +126,117 @@ export default {
   }
 </script>
 <style scoped>
-  #burger-table {
-    max-width: 100vw;
-    margin: 0 auto;
-  }
-  #burger-table-heading,
-  #burger-table-rows,
-  .burger-table-row {
+  .dash {
+    max-width: 100%;
     display: flex;
-    justify-content: space-between;
+    flex-wrap: wrap;
+    justify-content: center;
+    font-size: .8em;
+  }
+
+  .card {
+    width: 350px;
+    height: 460px;
+    background-color: #FFF;
+    margin: 20px 10px;
+    border: 2px solid #222;
+    border-radius: 10px;
+  }
+
+  .id {
+    background-color: #FCBA03;
+    border-radius: 9px 9px 0 0;
     text-align: center;
+    font-weight: bolder;
+    font-size: 1em;
+    padding: 5px;
+    border-bottom: #222 solid 2px;
+    display: flex;
+    justify-content: center;
   }
-  #burger-table-heading {
+
+  .id span {
+    margin-right: 10px;
     font-weight: bold;
-    padding-bottom: 10px;
-    border-bottom: 3px solid #333;
+    font-size: 1em;
   }
 
-  ul li {
-    list-style: none;
+  .name {
+    padding: 10px;
+    color: #222;
+    font-weight: bold;
+    font-size: 1em;
   }
 
-  .burger-table-row {
-    width: 100%;
-    padding: 10px 0px;
-    border-bottom: 1px solid #CCC;
+  .bread, .burger, .colunm, .name {
+    display: flex;
+    justify-content: space-around;
+    align-content: center;
     align-items: center;
   }
-  #burger-table-heading div,
-  .burger-table-row div {
-    width: 19%;
+
+  .main, .action {
+    display: flex;
+    justify-content: space-evenly;
+    align-content: center;
+    align-items: center;
   }
-  #burger-table-heading .order-id,
-  .burger-table-row .order-number {
-    width: 5%;
+
+  .breadType, .breadMode {
+    margin-top: 1em;
+    height: 50px;
+  }
+
+  .burgerType, .spices, .sauces, .coldCuts, .add, .salad {
+    height: 90px;
+  }
+
+  span {
+    font-weight: bold;
+    font-size: .8em;
+    padding-left: 10px;
+    border-left: 4px solid #fcba0398;
+  }
+
+  ul {
+    padding-left: 20px;
+  }
+
+  li {
+    list-style: circle;
   }
 
   .action {
-    display: flex;
-    justify-content: end;
-    justify-items: end;
-    margin: 10px
+    padding: 5px;
   }
 
-  .delete-btn {
+  .action button {
     background-color: #222;
     color:#fcba03;
     font-weight: bold;
-    border: 2px solid #222;
+    border: 1px solid #222;
+    margin-top: 10px;
     padding: 10px;
-    font-size: 16px;
-    margin-left: 10px;
     cursor: pointer;
     transition: .5s;
+    max-width: 200px;
+    border-radius: 10px
   }
-  
-  .delete-btn:hover {
+
+  .action button:hover {
     background-color: transparent;
     color: #222;
+  }
+
+  .action select {
+    background-color: transparent;
+    color: #222;
+    border: 1px solid #222;
+    margin-top: 10px;
+    padding: 10px;
+    cursor: pointer;
+    transition: .5s;
+    max-width: 200px;
+    border-radius: 5px
   }
 </style>
--- src/views/Requests.vue
+++ src/views/Requests.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main-container">
-    <h1>Gerenciar pedidos</h1>
+    <h1>Pedidos</h1>
     <Dashboard/>
   </div>
 </template>
@@ -17,6 +17,9 @@ export default {
 
 <style scoped>
   .main-container {
-    margin: 20px;
+    margin: 20px auto;
+  }
+  h1 {
+    margin: 0
   }
 </style>
