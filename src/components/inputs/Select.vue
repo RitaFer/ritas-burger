1d1b5592bc6f68b61dbaca9028f4b730db191314
Bruno Alfieri <alfieri@outlook.com.br>
2022-04-14 20:12:54 -0300

    create component input select

--- src/components/inputs/Select.vue
+++ src/components/inputs/Select.vue
@@ -1,13 +1,47 @@
 <template>
-
+  <div class="custom-select">
+    <label :for="id">{{ label }}</label>
+    <select :id="id" 
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)">
+      <option v-for="item in options"
+      :key="item.id" 
+      :value="item.id">
+        {{ item.type }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    modelValue: {
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: false
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .custom-select label {
+    display: block;
+  }
+  .custom-select select {
+    display: block;
+    width: 100%;
+    padding: 5px 10px;
+  }
 </style>
